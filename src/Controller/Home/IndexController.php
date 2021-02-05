717cd01aa16fbbf57a40cff4869b1ef513f775c1
François Gaillard <gailla.fr@gmail.com>
2021-02-05 22:05:02 +0100

    Add basic controller

---
+++ src/Controller/Home/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Home;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/", name="home")
+ */
+class IndexController extends AbstractController
+{
+    public function __invoke(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+}
