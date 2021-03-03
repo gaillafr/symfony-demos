4f74217318c2733743e5ccbb22041a1e17bc810f
François Gaillard <gailla.fr@gmail.com>
2021-03-03 23:04:56 +0100

    Add HttpClient decorator for test environment (record/replay)

---
+++ src/Service/HttpClientDecorator.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Helper\TestResponse;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+use Symfony\Contracts\HttpClient\ResponseStreamInterface;
+
+class HttpClientDecorator implements HttpClientInterface
+{
+    private HttpClientInterface $client;
+
+    public function __construct(HttpClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function request(string $method, string $url, array $options = []): ResponseInterface
+    {
+        // TODO: Check for existing recorded response.
+        return (new TestResponse())
+            ->setContent('{"ip": "0.0.0.0"}')
+        ;
+    }
+
+    /**
+     * @param iterable|ResponseInterface|ResponseInterface[] $responses
+     */
+    public function stream($responses, float $timeout = null): ResponseStreamInterface
+    {
+        return $this->client->stream($responses, $timeout);
+    }
+}
