33d51877ea10f2503568fdb8798d37824e6e24f9
François Gaillard <gailla.fr@gmail.com>
2021-03-04 17:50:36 +0100

    Add test

--- src/Service/HttpClientDecorator.php
+++ src/Service/HttpClientDecorator.php
@@ -11,6 +11,8 @@ use Symfony\Contracts\HttpClient\ResponseStreamInterface;
 
 class HttpClientDecorator implements HttpClientInterface
 {
+    private static int $expectedResponseCode;
+
     private HttpClientInterface $client;
 
     public function __construct(HttpClientInterface $client)
@@ -23,6 +25,8 @@ class HttpClientDecorator implements HttpClientInterface
      */
     public function request(string $method, string $url, array $options = []): ResponseInterface
     {
+        dump(self::$expectedResponseCode);
+        exit;
         // TODO: Check for existing recorded response.
         return (new TestResponse())
             ->setContent('{"ip": "0.0.0.0"}')
@@ -35,5 +39,10 @@ class HttpClientDecorator implements HttpClientInterface
     public function stream($responses, float $timeout = null): ResponseStreamInterface
     {
         return $this->client->stream($responses, $timeout);
+    }
+
+    public static function setExpectedResponseCode(int $code): void
+    {
+        self::$expectedResponseCode = $code;
     }
 }
--- tests/Functional/Home/IndexControllerTest.php
+++ tests/Functional/Home/IndexControllerTest.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace App\Tests\Functional\Home;
 
+use App\Service\HttpClientDecorator;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class IndexControllerTest extends WebTestCase
 {
     public function testIndexController(): void
     {
+        HttpClientDecorator::setExpectedResponseCode(500);
+
         $client = static::createClient();
         $client->request('GET', '/');
 
